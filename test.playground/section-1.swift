863c4c1ec2db68e52b9a421549ab4af5bed6d26f
basxeon <supachaixeon@gmail.com>
2015-01-16 16:52:21 +0700

    TestCode

---
+++ test.playground/section-1.swift
@@ -0,0 +1,58 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let num1 = 5
+let num2 = 10
+var total = num1 + num2
+
+print("\(total) = \(num1)+\(num2) ")
+
+total += 1
+let priceInferred = -19
+let priceIntExplicit:Double = -19.99
+
+let priceFloatInferred = 19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Bas Xeon"
+let nameExplicit:String = "Bas Xeon"
+
+var shopingList = ["Eggs","Milk"]
+var shopingListExplicit:[String] = ["Eggs","Milk"]
+print("The shopping list containts \(shopingList.count) items.")
+shopingList.append("Flour")
+shopingList += ["Baking Powder"]
+
+var firstitem = shopingList[0]
+
+shopingList[0] = "Six eggs"
+
+
+if (onSaleInferred) {
+    println("\(nameInferred) on sale for \(priceInferred)!")
+}else {
+    println("\(nameInferred) at regular price \(priceInferred)!")
+}
+
+
+var array = ["abc","bas","vbs"]
+for (var i = 0;i < array.count;i++){
+println(array[i])
+}
+
+var aa = ["A","C","V","S"]
+var dtr = ""
+for dtr in aa {
+println(dtr)
+}
+
+
+
+
+
+
